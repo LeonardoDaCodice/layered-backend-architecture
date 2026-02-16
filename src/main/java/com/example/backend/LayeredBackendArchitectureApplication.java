e871de72b9d3c4828abf2acbcee0c9ac0c21ac8c
LeonardoDaCodice <fortesalvatore333@gmail.com>
2026-02-16 19:12:39 +0100

    feat: bootstrap Spring Boot Maven project

---
+++ src/main/java/com/example/backend/LayeredBackendArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredBackendArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeredBackendArchitectureApplication.class, args);
+	}
+
+}
